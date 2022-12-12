7893a75ee792de2f397d03fb0b43da89163fda88
HKazuk1 <cbkh1210452@gn.iwasaki.ac.jp>
2022-12-13 01:15:13 +0900

    commit ABC203 c.py

---
+++ ABC/ABC203/c.py
@@ -0,0 +1,29 @@
+import io
+import sys
+_INPUT = """\
+3 2
+5 5
+2 1
+2 2
+"""
+sys.stdin = io.StringIO(_INPUT)
+
+n, k = map(int, input().split())
+mat = []
+
+for _ in range(n):
+    a, b = map(int, input().split())
+    mat.append((a, b))
+
+# aを小さい順にソート
+mat.sort()
+now = k # 始めの所持金で村Kまで行ける
+for i in range(n):
+    a, b = mat[i]
+    if now >= a:
+        # 村A_iの人がB_i円 くれるので、即座に消費してB_i進む
+        now += b
+    else:
+        break
+
+print(now)
