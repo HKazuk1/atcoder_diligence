2be8b2cdd735fb7e6068d6439589b9a7816bfb6f
HKazuk1 <cbkh1210452@gn.iwasaki.ac.jp>
2023-01-07 18:26:22 +0900

    fix ABC224 B

--- ABC/ABC224/b.py
+++ ABC/ABC224/b.py
@@ -11,10 +11,10 @@ sys.stdin = io.StringIO(_INPUT)
 H, W =map(int, input().split())
 A = [list(map(int, input().split())) for _ in range(H)]
 
-for i in range(W-1):
-    for ii in range(i+1, W):
-        for j in range(H-1):
-            for jj in range(j+1, H):
+for i in range(H):
+    for ii in range(i+1, H):
+        for j in range(W):
+            for jj in range(j+1, W):
                 if A[i][j] + A[ii][jj] > A[ii][j] + A[i][jj]:
                     print("No")
                     exit()
