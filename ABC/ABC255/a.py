a0f9369010fe1420cff83c63259910b4cd32b127
HKazuk1 <cbkh1210452@gn.iwasaki.ac.jp>
2023-02-20 01:54:56 +0900

    commit ABC255 A

---
+++ ABC/ABC255/a.py
@@ -0,0 +1,13 @@
+import io
+import sys
+_INPUT = """\
+2 2
+1 2
+3 4
+"""
+sys.stdin = io.StringIO(_INPUT)
+
+R, C = map(int, input().split())
+A = [list(map(int, input().split())) for _ in range(2)]
+
+print(A[R-1][C-1])
